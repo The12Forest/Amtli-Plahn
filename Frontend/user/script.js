86d87c4f4b50ffb7800f4a1681b76403e748b2ee
The12Forest <manuel@steka.ch>
2025-05-28 18:02:37 +0200

    updated urls

--- Frontend/user/script.js
+++ Frontend/user/script.js
@@ -1,6 +1,6 @@
 let userName
-// let basedomain = "https://server5.techsvc.de:2007"
-let basedomain = "https://localhost:2007"
+let basedomain = "https://server5.techsvc.de:2007"
+// let basedomain = "https://localhost:2007"
 
 
 fetch(basedomain + "/api/temp/get")
