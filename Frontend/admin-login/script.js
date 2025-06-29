719767794e772feb6b69d56111c0a5a8fb1b22a6
The12Forest <manuel@steka.ch>
2025-06-29 18:12:14 +0200

    Somme tipos

--- Frontend/admin-login/script.js
+++ Frontend/admin-login/script.js
@@ -1,5 +1,5 @@
-// let baseurl = "https://server5.techsvc.de:2007"
-let baseurl = "https://localhost:2007"
+let baseurl = "https://server5.techsvc.de:2007"
+// let baseurl = "https://localhost:2007"
 
 
 document.querySelector(".login-form").addEventListener("submit", async (e) => {
